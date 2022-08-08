68474b98004eb6987c2a9a733648d6cfc0ec3afd
Cole Helbling <cole.helbling@determinate.systems>
2022-08-08 07:45:47 -0700

    flake: init

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "fsm";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self
+    , nixpkgs
+    , ...
+    } @ inputs:
+    let
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+      allSystems = [ "x86_64-linux" "aarch64-linux" "i686-linux" "x86_64-darwin" ];
+
+      forAllSystems = f: genAttrs allSystems (system: f {
+        inherit system;
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShell = forAllSystems ({ system, pkgs, ... }:
+        pkgs.mkShell {
+          name = "fsm-shell";
+
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            clippy
+            codespell
+            nixpkgs-fmt
+            rustfmt
+          ];
+        });
+
+      packages = forAllSystems
+        ({ system, pkgs, ... }:
+          {
+            package = pkgs.rustPlatform.buildRustPackage rec {
+              pname = "fsm";
+              version = "unreleased";
+              src = self;
+
+              preBuild = ''
+                cargo clippy --all-targets --all-features -- -D warnings
+              '';
+
+              cargoLock.lockFile = ./Cargo.lock;
+            };
+          });
+
+      defaultPackage = forAllSystems ({ system, ... }: self.packages.${system}.package);
+    };
+}
