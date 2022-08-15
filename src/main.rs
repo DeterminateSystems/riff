de8de546e3d413d8998b89dba26667a980fb468d
Cole Helbling <cole.helbling@determinate.systems>
2022-08-15 12:22:21 -0700

    Shell out to `cargo metadata` instead of linking to the cargo crate as a library
    This allows us to have an actually-successful build of fsmStatic without
    weirdness due to openssl.

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ async fn cmd_shell(shell_args: Shell) -> color_eyre::Result<()> {
 
     let mut dev_env = DevEnvironment::default();
 
-    dev_env.detect(&project_dir)?;
+    dev_env.detect(&project_dir).await?;
 
     let flake_nix = dev_env.to_flake();
 
@@ -106,6 +106,7 @@ async fn cmd_shell(shell_args: Shell) -> color_eyre::Result<()> {
         .args(&["--extra-experimental-features", "flakes nix-command"])
         .arg("-L")
         .arg(format!("path://{}", flake_dir.path().to_str().unwrap()));
+
     tracing::trace!(command = ?nix_lock_command, "Running");
     let nix_lock_exit = nix_lock_command
         .output()
@@ -133,6 +134,7 @@ async fn cmd_shell(shell_args: Shell) -> color_eyre::Result<()> {
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit());
+
     tracing::trace!(command = ?nix_develop_command, "Running");
     let nix_develop_exit = nix_develop_command
         .spawn()
@@ -174,11 +176,11 @@ impl DevEnvironment {
         )
     }
 
-    fn detect(&mut self, project_dir: &Path) -> color_eyre::Result<()> {
+    async fn detect(&mut self, project_dir: &Path) -> color_eyre::Result<()> {
         let mut any_found = false;
 
         if project_dir.join("Cargo.toml").exists() {
-            self.add_deps_from_cargo(project_dir)?;
+            self.add_deps_from_cargo(project_dir).await?;
             any_found = true;
         }
 
@@ -193,7 +195,7 @@ impl DevEnvironment {
     }
 
     #[tracing::instrument(skip_all, fields(project_dir = %project_dir.display()))]
-    fn add_deps_from_cargo(&mut self, project_dir: &Path) -> color_eyre::Result<()> {
+    async fn add_deps_from_cargo(&mut self, project_dir: &Path) -> color_eyre::Result<()> {
         // Mapping of `$CRATE_NAME -> $NIXPKGS_NAME`
         static KNOWN_CRATE_TO_BUILD_INPUTS: Lazy<HashMap<&'static str, HashSet<&'static str>>> =
             Lazy::new(|| {
@@ -222,54 +224,42 @@ impl DevEnvironment {
         found_build_inputs.insert("rustc".to_string());
         found_build_inputs.insert("cargo".to_string());
 
-        let mut cfg = cargo::util::config::Config::default()
-            .map_err(|e| eyre!(e))
-            .wrap_err("Could not get default `cargo` instance")?;
+        let mut cmd = Command::new("cargo");
+        cmd.args(&["metadata", "--format-version", "1"]);
+        cmd.arg("--manifest-path");
+        cmd.arg(project_dir.join("Cargo.toml"));
 
-        // TODO(@hoverbear): Add verbosity option
-        cfg.configure(
-            0,     // verbose
-            true,  // quiet
-            None,  // color
-            false, // frozen
-            false, // locked
-            false, // offline
-            &None, // target_dir
-            &[],   // unstable_flags
-            &[],   // cli_config
-        )
-        .map_err(|e| eyre!(e))
-        .wrap_err("Could not configure `cargo`")?;
+        let output = cmd.output().await?;
+        if !output.status.success() {
+            todo!("statuscode error");
+        }
 
-        let workspace = cargo::core::Workspace::new(&project_dir.join("Cargo.toml"), &cfg)
-            .map_err(|e| eyre!(e))
-            .wrap_err_with(|| {
-                format!(
-                    "Could not create workspace from `{}`",
-                    project_dir.display()
-                )
-            })?;
+        #[derive(serde::Deserialize)]
+        struct CargoMetadata {
+            packages: Vec<Package>,
+        }
 
-        let (package_set, resolve) = cargo::ops::resolve_ws(&workspace)
-            .map_err(|e| eyre!(e))
-            .wrap_err_with(|| {
-                format!(
-                    "Could not resolve workspace from `{}`",
-                    project_dir.display()
-                )
-            })?;
+        // TODO: impl deserialize manually so we can make name be a String and metadata be an Object?
+        #[derive(serde::Deserialize)]
+        struct Package {
+            name: serde_json::Value,
+            metadata: serde_json::Value,
+        }
 
-        for package in package_set.get_many(resolve.iter()).unwrap() {
-            let mut package_build_inputs = HashSet::new();
+        let stdout = std::str::from_utf8(&output.stdout)?;
+        let metadata: CargoMetadata = serde_json::from_str(stdout)?;
 
-            if let Some(known_build_inputs) =
-                KNOWN_CRATE_TO_BUILD_INPUTS.get(package.name().as_str())
-            {
+        for package in metadata.packages {
+            let name = package.name.as_str().unwrap(); // FIXME
+            let mut package_build_inputs: HashSet<String> = HashSet::new();
+
+            if let Some(known_build_inputs) = KNOWN_CRATE_TO_BUILD_INPUTS.get(name) {
                 let known_build_inputs = known_build_inputs
                     .iter()
                     .map(ToString::to_string)
                     .collect::<HashSet<_>>();
-                tracing::debug!(package_name = %package.name(), inputs = %known_build_inputs.iter().join(", "), "Detected known build inputs");
+
+                tracing::debug!(package_name = %name, inputs = %known_build_inputs.iter().join(", "), "Detected known build inputs");
                 found_build_inputs = found_build_inputs
                     .union(&known_build_inputs)
                     .cloned()
@@ -277,33 +267,27 @@ impl DevEnvironment {
             }
 
             // TODO(@hoverbear): Add a `Deserializable` implementor we can get from this.
-            let custom_metadata = match package.manifest().custom_metadata() {
-                Some(custom_metadata) => custom_metadata,
-                None => continue,
-            };
-
-            let metadata_table = match custom_metadata {
-                toml_edit::easy::value::Value::Table(metadata_table) => metadata_table,
+            let metadata_object = match package.metadata {
+                serde_json::Value::Object(metadata_object) => metadata_object,
                 _ => continue,
             };
 
-            let fsm_table = match metadata_table.get("fsm") {
-                Some(toml_edit::easy::value::Value::Table(metadata_table)) => metadata_table,
+            let fsm_object = match metadata_object.get("fsm") {
+                Some(serde_json::Value::Object(fsm_object)) => fsm_object,
                 Some(_) | None => continue,
             };
 
-            let build_inputs_table = match fsm_table.get("build-inputs") {
-                Some(toml_edit::easy::value::Value::Table(build_inputs_table)) => {
-                    build_inputs_table
-                }
+            let build_inputs_object = match fsm_object.get("build-inputs") {
+                Some(serde_json::Value::Object(build_inputs_object)) => build_inputs_object,
                 Some(_) | None => continue,
             };
 
-            for (key, _value) in build_inputs_table.iter() {
+            for (key, _value) in build_inputs_object.iter() {
                 // TODO(@hoverbear): Add version checking
                 package_build_inputs.insert(key.to_string());
             }
-            tracing::debug!(package_name = %package.name(), inputs = %package_build_inputs.iter().join(", "), "Detected `package.fsm.build-inputs` in `Crate.toml`");
+
+            tracing::debug!(package_name = %name, inputs = %package_build_inputs.iter().join(", "), "Detected `package.fsm.build-inputs` in `Crate.toml`");
             found_build_inputs = found_build_inputs
                 .union(&package_build_inputs)
                 .cloned()
