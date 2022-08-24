ea24c600858c52d8e4834b80eee0c8aaf7a0ba28
Graham Christensen <graham@grahamc.com>
2022-08-24 13:49:53 -0400

    Revert "Don't spawn and then wait for output in separate steps"
    This commit appears to have caused a few problems where shell blocks forever
    and `run` never outputs anything.
    
    This reverts commit 7cfe53b434dd6aee7e4cbba4e5e6a61480e099b9.

--- src/cmds/run.rs
+++ src/cmds/run.rs
@@ -47,7 +47,9 @@ impl Run {
 
         tracing::trace!(command = ?nix_develop_command, "Running");
         let nix_develop_exit = nix_develop_command
-            .output()
+            .spawn()
+            .wrap_err("Failed to spawn `nix develop`")?
+            .wait_with_output()
             .await
             .wrap_err("Could not execute `nix develop`")?;
 
