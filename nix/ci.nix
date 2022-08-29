79a63ad79603a90affad0e41d1d700360773db45
Luc Perkins <lucperkins@gmail.com>
2022-08-29 23:32:57 +0300

    Add comments to scripts to clarify relationship with CI

--- nix/ci.nix
+++ nix/ci.nix
@@ -4,7 +4,14 @@ let
   inherit (pkgs) writeScriptBin;
 in
 [
-  # Check spelling
+
+  # Format
+  (writeScriptBin "ci-check-rustfmt" "cargo fmt --check")
+
+  # Test
+  (writeScriptBin "ci-test-rust" "cargo test")
+
+  # Spelling
   (writeScriptBin "ci-check-spelling" ''
     codespell \
       --ignore-words-list crate,pullrequest,pullrequests,ser \
@@ -12,28 +19,25 @@ in
       .
   '')
 
-  # Rust formatting check
-  (writeScriptBin "ci-check-rustfmt" "cargo fmt --check")
-
-  # Rust test
-  (writeScriptBin "ci-test-rust" "cargo test")
-
+  # NixFormatting
   (writeScriptBin "ci-check-nixpkgs-fmt" ''
     git ls-files '*.nix' | xargs | nixpkgs-fmt --check
   '')
 
+  # RegistryFormatting
   (writeScriptBin "ci-check-registry-format" ''
     ./registry/format.sh && git diff --exit-code
   '')
 
+  # EditorConfig
   (writeScriptBin "ci-check-editorconfig" ''
     eclint
   '')
 
   (writeScriptBin "ci-all" ''
-    ci-check-spelling
     ci-check-rustfmt
     ci-test-rust
+    ci-check-spelling
     ci-check-nixpkgs-fmt
     ci-check-registry-format
     ci-check-editorconfig
