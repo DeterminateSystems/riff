c1a4de9a4097d51e6d40746c08f8684c269ddfd7
Graham Christensen <graham@grahamc.com>
2022-08-31 09:41:03 -0400

    Switch the loading unicode symbols to, uh, different ones
    The old ones would smush on top of each other when rendered by
    fancier renderers. These seem to render better, but it may be
    that we just can't use unicode in our loader.
    
    Another change is I made the tick interval 260ms to match the
    high-hat I have in my head:
    
    (115 beats per minute) / (60 seconds) = .52s per quarter note
    = .26 seconds per eighth note
    = 260ms per eighth note
    
    Note that I also double-render the quarter note so it is
    "played" for the correct amount of time.

--- src/spinner.rs
+++ src/spinner.rs
@@ -2,18 +2,30 @@ use std::time::Duration;
 
 use indicatif::{ProgressBar, ProgressStyle};
 
+const LEADER: char = ' ';
+const DRUM: char = '🥁';
+const QUARTER_NOTE: char = '♩';
+const EIGHTH_NOTE: char = '♪';
+
 pub struct SimpleSpinner;
 
 impl SimpleSpinner {
     pub fn new_with_message(msg: Option<&str>) -> color_eyre::Result<ProgressBar> {
         let spinner = ProgressBar::new_spinner();
-        spinner.enable_steady_tick(Duration::from_millis(500));
+        spinner.enable_steady_tick(Duration::from_millis(260));
         spinner.set_style(
             ProgressStyle::with_template("{msg}{spinner}")?.tick_strings(&[
-                " 🥁 𝄥𝅘𝅥   ",
-                " 🥁 𝄥𝅘𝅥𝅘𝅥𝅮  ",
-                " 🥁 𝄥𝅘𝅥𝅘𝅥𝅮𝅘𝅥𝅮 ",
-                " 🥁 𝄥𝅘𝅥𝅘𝅥𝅮𝅘𝅥𝅮 ",
+                &([LEADER, DRUM, QUARTER_NOTE].into_iter().collect::<String>()),
+                &([LEADER, DRUM, QUARTER_NOTE].into_iter().collect::<String>()),
+                &([LEADER, DRUM, QUARTER_NOTE, EIGHTH_NOTE]
+                    .into_iter()
+                    .collect::<String>()),
+                &([LEADER, DRUM, QUARTER_NOTE, EIGHTH_NOTE, EIGHTH_NOTE]
+                    .into_iter()
+                    .collect::<String>()),
+                &([LEADER, DRUM, QUARTER_NOTE, EIGHTH_NOTE, EIGHTH_NOTE]
+                    .into_iter()
+                    .collect::<String>()),
             ]),
         );
 
