3a5420ae628a6e0309ae0d248b0e1e0d68711b9f
Linus Heckemann <git@sphalerite.org>
2022-09-27 18:51:49 +0200

    remove some currently still unused parts from metadata
    The warning is fatal when building within Nix.

--- src/metadata/go.rs
+++ src/metadata/go.rs
@@ -8,10 +8,10 @@ pub(crate) struct GoPackage {
     pub(crate) dir: PathBuf,
     #[serde(rename = "ImportPath")]
     pub(crate) import_path: String,
-    #[serde(rename = "CgoCFLAGS")]
-    pub(crate) cgo_cflags: Option<Vec<String>>,
-    #[serde(rename = "CgoLDFLAGS")]
-    pub(crate) cgo_ldflags: Option<Vec<String>>,
-    #[serde(rename = "CgoPkgConfig")]
-    pub(crate) cgo_pkg_config: Option<Vec<String>>,
+    // #[serde(rename = "CgoCFLAGS")]
+    // pub(crate) cgo_cflags: Option<Vec<String>>,
+    // #[serde(rename = "CgoLDFLAGS")]
+    // pub(crate) cgo_ldflags: Option<Vec<String>>,
+    // #[serde(rename = "CgoPkgConfig")]
+    // pub(crate) cgo_pkg_config: Option<Vec<String>>,
 }
